703260d40245bacff0baeae3ec6c87b2ebed5e15
victor cardona <vc787259@gmail.com>
2024-03-14 06:39:20 -0400

    asdasdasdasdasdas

--- proyecto/conexion.js
+++ proyecto/conexion.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import mysql from 'mysql2/promise'; // Importa mysql2/promise para soporte de promesas
+import mysql from 'mysql'; // Importa mysql2/promise para soporte de promesas
 
 import { fileURLToPath } from 'url';
 
@@ -14,73 +14,81 @@ const PORT = process.env.PORT || 3000;
 let conexion;
 
 async function connectDatabase() {
- try {
-    conexion = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "actividades_incidencias"
-    });
-    console.log('Conexión a la base de datos establecida');
- } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
-    process.exit(1);
- }
-}
+   try {
+      conexion = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "actividades_incidencias"
+      });
+      console.log('Conexión a la base de datos establecida');
+   } catch (error) {
+      console.error('Error al conectar a la base de datos:', error);
+      process.exit(1);
+   }
+  }
+  connectDatabase();
 
-connectDatabase();
+  app.use(express.static(path.join(__dirname, 'dist')));
 
-// Sirve los archivos estáticos de la aplicación React
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Asegúrate de que todas las solicitudes se redirijan al archivo index.html
-// para que React Router funcione correctamente
 app.get('*', (req, res) => {
  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Insertar incidentes a la base de datos
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", function(req,res){
+    res.render("validar");
+})
+app.get('/nose', async (req, res) => {
 
 
-app.get('/seleccion', async (req, res) => {
- try {
-    const [results] = await conexion.execute('SELECT * FROM incidentes');
-    res.json(results);
- } catch (error) {
-    console.error('Error al obtener incidentes:', error);
-    res.status(500).send('Error al obtener incidentes');
- }
-});
+  // Consulta SELECT
+  const actividades_ejecucion = await conexion.query('SELECT * FROM actividades_ejecucion');
 
-app.post("/validar", async (req, res) => {
- const { incidente, descripcion, fecha, hora, ubicacion,actividad , descripcionac, ubicacionac, horaci, horacf, pt } = req.body;
+  // Manejo de errores
+  if (actividades_ejecucion.error) {
+    throw actividades_ejecucion.error;
+  }
+console.log(actividades_ejecucion);
+  // Enviar datos al front-end
+  res.json(actividades_ejecucion);
+ });
 
- try {
-    await conexion.execute(
-      'INSERT INTO incidentes(descripcion_incidente, Paquete_Turistico_id_Paquete_Turistico, tipo_incidencia_id_tipo_incidencia, fecha, hora, ubicacion) VALUES (?, ?, ?, ?, ?, ?)',
-      [descripcion, '1', incidente, fecha, hora, ubicacion]
-    );
-    console.log("Registro exitoso");
-    res.status(200).send('Registro exitoso');
- } catch (error) {
-    console.error('Error al registrar incidente:', error);
-    res.status(500).send('Error al registrar incidente');
- }
-   await conexion.execute(
-     'INSERT INTO `actividades_ejecucion`( `actividad`, `descripcion_actividad`, `lugar`, `hora_incio`, `hora_fin`, `Paquete_Turistico_id_Paquete_Turistico`,  ) VALUES (?,?,?,?,?,?)',
-     [actividad, descripcionac, ubicacionac, horaci, horacf, pt]
-   );
-   console.log("Registro exitoso :)");
-   res.status(200).send('Registro exitoso');
- } catch (error) {
-   console.error('Error al registrar actividad:', error);
-   res.status(500).send('Error al registrar actividad');
- }
-});
 
+
+
+app.post("/regiac",function(req,res)
+  {
+      const { catego,actividad, descripcionac, ubicacionac, horaci, horacf, pt } = req.body;
+      let inyec="INSERT INTO `actividades_ejecucion`(  `actividad`, `descripcion_actividad`, `lugar`, `hora_incio`, `hora_fin`, `Paquete_Turistico_id_Paquete_Turistico`, `categoria_id_categoria`, `Bitacora_id_Bitacora`, `cumplimiento_id_cumplimiento`) VALUES ('"+actividad+"','"+descripcionac+"','"+ubicacionac+"','"+horaci+"','"+horacf+"','"+pt+"','"+catego+"','1','1')"
+      conexion.query(inyec,function(error){
+          if(error){
+              console.log(error);
+          }else{
+              console.log("Registro exitoso");
+          }      
+      });
+  }
+)
+app.post("/validar",function(req,res)
+
+  {
+    const { incidente, descripcion, fecha, hora, ubicacion } = req.body;
+    let inyec="INSERT INTO `incidentes`( `descripcion_incidente`, `Paquete_Turistico_id_Paquete_Turistico`, `tipo_incidencia_id_tipo_incidencia`, `fecha`, `hora`, `ubicacion`) VALUES ('"+descripcion+"','1','"+incidente+"','"+fecha+"','"+hora+"','"+ubicacion+"')"
+    conexion.query(inyec,function(error){
+      if(error){
+          console.log(error);
+      }else{
+          console.log("Registro exitoso");
+      }      
+  });
+  })
+  
+  
 app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
-});
+   console.log(`Server running on port ${PORT}`)
+  });
+  
+  
--- proyecto/src/pages/actrividad.jsx
+++ proyecto/src/pages/actrividad.jsx
@@ -1,54 +1,53 @@
-import { useState } from "react";
-import {
-  Modal,
-  Table,
-  ModalFooter,
-  ModalBody,
-  ModalHeader,
-  Label,
-  FormGroup,
-  Input,
-  Option,
-  Button
-} from "reactstrap";
-import "../styles/incidetes.css"
-export default function Actividad(args) {
-    const [modal, setModal] = useState(false);
-  const toggle = () => {setModal(!modal); }
 
-  const handleSubmit = () => {
-    
-    toggle(); 
-    // Add any additional logic for handling
-  };
-  return(
-    <>
-    <div >
-        <div className="ta">
-            <h1>Actividades</h1>
-        <Table>
-                <thead>
-                    <tr className="table-dark">
-                        <th className="p-4">Actividad</th>
-                        <th className="p-4">d</th>
-                        <th className="p-4">Fecha</th>
-                        <th className="p-4">Hora</th>
-                        <th className="p-4">cumplimiento</th>
-                        <th className="p-4">Acción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                       <td></td>
-                       <td></td>
-                       <td></td>
-                       <td></td>
-                       <td></td>
-                    </tr>
-                </tbody>
-            </Table>
-            <Button onClick={toggle} color="primary">agregar</Button>
-            </div>
+
+import { useState, useEffect } from "react";
+
+import { Modal, ModalFooter,Table, ModalBody, ModalHeader, Label, FormGroup, Input, Button } from 'reactstrap';
+import "../styles/incidetes.css";
+
+export default function Actividad(args) {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+  const [actividades, setActividades] = useState([]);
+
+  useEffect(() => {
+    // Petición GET a la API
+    fetch('http://localhost:3000/actividades')
+      .then((response) => response.json())
+      .then((data) => {
+        setActividades(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching activities:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+    <div>
+      <h1>Actividades</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Actividad</th>
+            <th>Descripción</th>
+            <th>Ubicación</th>
+          </tr>
+        </thead>
+        <tbody>
+        {actividades.map((actividad) => (
+  <tr key={actividad.id}> {/* Agrega la propiedad key con un valor único */}
+    <td>{actividad.actividad}</td>
+    <td>{actividad.descripcion_actividad}</td>
+    <td>{actividad.ubicacion}</td>
+  </tr>
+))}
+        </tbody>
+      </Table>
+    </div>
+
+          <Button onClick={toggle} color="primary">Agregar</Button>
+        <div>
             <Modal
         isOpen={modal}
         toggle={toggle}
@@ -56,20 +55,24 @@ export default function Actividad(args) {
         centered
         backdropClassName="custom-backdrop-ac" // Clase personalizada para el backdrop
         contentClassName="custom-modal-content-ac">
-        <form action="/validar" method="post">
+        <form action="/regiac" method="post">
         <ModalHeader className="custom-modalHead-ac" toggle={toggle}>
           <h3>Registrar Incidente</h3>
         </ModalHeader>
         <ModalBody>
             <div className="contain">
           <FormGroup>
-              <Label>Actividad</Label>
-              <Input type="select" name="actividad">
+              <Label>Categoria</Label>
+              <Input type="text" name="catego">
                <option value="1">deportiva</option>
                <option value="2">recreacion</option>
                <option value="3">alimentacion</option>
                <option value="4">riesgo</option>
               </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label> Actividad:</Label>
+
           </FormGroup>
           <FormGroup>
             <Label>Descripción</Label>
@@ -106,6 +109,7 @@ export default function Actividad(args) {
         </form>
       </Modal>
         </div>
-        </>
+        </div>
+        
   )
 }
